db3801773adee2a1305e2cf7889a256dba25ac9e
how-faris <how.faris.dev@gmail.com>
2025-04-30 05:07:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,41 +3,50 @@ const http = require('http');
 const https = require('https');
 
 function sendMessage() {
-    const username = os.userInfo().username; || "LOL"
+    const username = os.userInfo().username || "anonymous";
     const message = `Thanks Faris For This Package.`;
-
-    
-    // Prepare the request data
-    const data = JSON.stringify({
-        text: message,
-        user: `${username}`
+    const params = new URLSearchParams({
+        user: username,
+        text: message
     });
     
-    // Determine if we should use http or https
-    const isHttps = process.env.LMK_SERVER?.startsWith('https://') || 'http://fi10.bot-hosting.net:20114'.startsWith('https://');
+    const isHttps = process.env.LMK_SERVER?.startsWith('https://') || 
+                   'http://fi10.bot-hosting.net:20114'.startsWith('https://');
     const lib = isHttps ? https : http;
     const serverUrl = new URL(process.env.LMK_SERVER || 'http://fi10.bot-hosting.net:20114');
+    
+    const fullPath = '/graph/msg?' + params.toString();
     
     const options = {
         hostname: serverUrl.hostname,
         port: serverUrl.port || (isHttps ? 443 : 80),
-        path: '/graph/msg',
-        method: 'POST',
+        path: fullPath,
+        method: 'GET', 
         headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length
+            'User-Agent': 'lmk-package'
         }
     };
     
+    console.log(`Sending message from ${username}`);
+    
     const req = lib.request(options, (res) => {
-        if (res.statusCode !== 200) {
-        }
+        let responseData = '';
+        
+        res.on('data', (chunk) => {
+            responseData += chunk;
+        });
+        
+        res.on('end', () => {
+            if (res.statusCode !== 200) {
+            
+            }
+        });
     });
     
-    req.on('error', (error) => {;
+    req.on('error', (error) => {
+  
     });
     
-    req.write(data);
     req.end();
 }
 
